c2b1ec8987d58186caf541fc4c794c63b8ee0f40
Shafiulislam <ishafiul900@gmail.com>
2023-05-03 09:45:48 +0600

    complit home page design section

--- src/pages/Home/ChefCard/ChafCart.jsx
+++ src/pages/Home/ChefCard/ChafCart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Card, Col, } from 'react-bootstrap';
+import { Button, Card, Col, } from 'react-bootstrap';
+import { FaThumbsUp } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ChafCart = ({ chef }) => {
-    const { _id, title, number_of_recipes, thumbnail_url, country, details, author } = chef;
+    const { _id, title, total_view, experience, number_of_recipes, thumbnail_url, country, details, author } = chef;
     return (
         <Col lg={6}>
             <Card className='mb-4'>
@@ -14,10 +16,12 @@ const ChafCart = ({ chef }) => {
                     </div>
                     <Card.Text>
                         <p>Her Recipes:- {number_of_recipes}</p>
+                        <p>Exprience {experience} Year</p>
                     </Card.Text>
                 </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
+                <Card.Footer className='d-flex justify-content-between   align-items-center'>
+                    <p className='d-flex align-items-center justify-content-center'> <span></span> <FaThumbsUp> </FaThumbsUp>{total_view}</p>
+                    <Link to={`/chef/${_id}`}><Button variant="secondary">view Details</Button></Link>
                 </Card.Footer>
             </Card>
         </Col>
