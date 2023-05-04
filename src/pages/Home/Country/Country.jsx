6111ed436141042bb42752100352c5932ddef606
Shafiulislam <ishafiul900@gmail.com>
2023-05-04 22:08:56 +0600

    complit

--- src/pages/Home/Country/Country.jsx
+++ src/pages/Home/Country/Country.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
 import ChafCart from '../ChefCard/ChafCart';
 import { Row } from 'react-bootstrap';
 
+
 const Country = () => {
     const { id } = useParams();
     const countryChef = useLoaderData();
+    const [set, setSet] = useState('')
+    console.log(countryChef)
+
+    useEffect(() => {
+        countryChef.findIndex(x =>  setSet(x.country));
+       },[])
     return (
         <div>
-            {id && <h2>Coutry {countryChef.length}</h2>}
+            {id && <h2>All Chef Country{countryChef.length}</h2>}
             <Row>
                 {
                     countryChef.map(chef => <ChafCart
